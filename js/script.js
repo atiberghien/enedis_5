a5148980712c5a9f2944e17ddef8dea4b3581a89
rruellepetel <ruellepetelromain@gmail.com>
2017-09-06 13:48:06 +0200

    fix setView() and maxZoom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-var mymap = L.map('mapid').setView([44.516787291861, 3.497662300000002], 15.7);
+var mymap = L.map('mapid').setView([44.51802660193136, 3.501524680981447], 15.5);
 mymap.pm.addControls(options);
 
 // define toolbar options
@@ -16,7 +16,7 @@ var options = {
 
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 20,
+    maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
